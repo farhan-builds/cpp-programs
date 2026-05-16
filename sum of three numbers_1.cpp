b586c5b2bcf755d77c75cc1fdccd880229716945
farhan-builds <farhanmondal20007@gmail.com>
2026-05-16 19:15:46 +0530

    Add files via upload

---
+++ sum of three numbers_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main ()
+{
+	int a,b,c,d;
+	std::cout << "Enter three numbers: ";
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	d=a+b+c;
+	std::cout << d;
+	return 0;
+	
+}
