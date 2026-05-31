87de7fc4e47605b84b3f2cb16fe05b9720cb3772
farhan-builds <farhanmondal20007@gmail.com>
2026-05-31 09:33:52 +0530

    Add files via upload

---
+++ even or odd_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << " Enter a number: ";
+	cin >> a;
+	if ( a % 2== 0 )
+	cout << " even number: ";
+	else
+		cout << " odd number: ";
+	return 0;
+	
+}
