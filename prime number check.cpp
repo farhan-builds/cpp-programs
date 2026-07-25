16cbc8a32d51313503036477f80cf073fb290aab
farhan-builds <farhanmondal20007@gmail.com>
2026-07-25 19:20:04 +0530

    Add files via upload

---
+++ prime number check.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,i;
+	cout << " enter a number ";
+	cin >> n;
+	for(i=2;  i<n;  i++)
+	{
+		if(n%i==0)
+		{
+			cout << " prime ";
+			return 0;
+		}
+	}
+	cout << " not prime ";
+	return 0;
+}
