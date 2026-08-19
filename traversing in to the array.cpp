be60bfccaa933a4fca53332973912edb03e4c59b
farhan-builds <farhanmondal20007@gmail.com>
2026-08-19 19:49:45 +0530

    Add files via upload

---
+++ traversing in to the array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int a[2][3] = {{1,2,3},{4,5,6}};
+	
+	for(int x = 0 ;x < 2;x++)
+	{
+		for(int y =0 ; y < 3; y++)
+		{
+			cout << a[x][y] << " ";
+		}
+		cout <<endl;
+	}
+	
+	return 0;
+	
+}
